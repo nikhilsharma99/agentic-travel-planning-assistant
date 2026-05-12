4efebb23c9365d25c7344c31842ab4d8759e00bb
nikhilsharma99 <nikhilsharmavp52@gmail.com>
2026-05-12 20:20:52 +0200

    Update package metadata

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ print(get_requirements())
 setup(
     name="AI-TRAVEL-PLANNER",
     version="0.0.1",
-    author="sunny savita",
-    author_email="snshrivas3365@gmail.com",
+    author="Nikhil Sharma",
+    author_email="sharmasnikhilde@gmail.com",
     packages = find_packages(),
     install_requires=get_requirements()
 )
